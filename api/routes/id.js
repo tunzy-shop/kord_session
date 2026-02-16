a74efef49383152620fb685964c8ef2cd7956395
tunzy-shop <tunzyprivate@gmail.com>
2026-02-16 11:40:55 +0100

    Create id.js

---
+++ api/routes/id.js
@@ -0,0 +1,11 @@
+function kordid(num = 4) {
+  let result = ""
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  var characters9 = characters.length
+  for (var i = 2; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters9))
+  }
+  return result
+}
+
+export { kordid }
